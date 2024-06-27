adf61934343462e0301a337eb57d53846b271d7a
Anton Hirov <hirov.anton@yahoo.com>
2024-06-27 13:57:53 +0300

    Added desktop tests project

---
+++ Tests/Desktop/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="App.xaml.cs" company="Anton Hirov - Private entrepreneur">
+// Copyright (c) Anton Hirov - Private entrepreneur. All rights reserved.
+// </copyright>
+
+namespace DarkgridToolkit.Desktop.Tests
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml.
+    /// </summary>
+    public partial class App : Application
+    {
+        // Application settings
+    }
+}
