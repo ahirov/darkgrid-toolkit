6c7012aa16ec03d303428b3b3984a3f7e8a908ee
Anton Hirov <hirov.anton@yahoo.com>
2024-12-25 14:45:01 +0200

    Added new Qt projects. Updated dependencies

---
+++ Qt/Qss/Program.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="Program.cs" company="Anton Hirov - Private entrepreneur">
+// Copyright (c) Anton Hirov - Private entrepreneur. All rights reserved.
+// </copyright>
+
+namespace DarkgridToolkit.Qt.Qss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tests/Qt/Qss/UnitTest1.cs
@@ -0,0 +1,20 @@
+// <copyright file="UnitTest1.cs" company="Anton Hirov - Private entrepreneur">
+// Copyright (c) Anton Hirov - Private entrepreneur. All rights reserved.
+// </copyright>
+
+namespace DarkgridToolkit.Qt.Qss.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
